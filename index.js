f25e41518bb96bfeaaea3ca9218091f1148c7953
Simon Lydell <simon.lydell@gmail.com>
2022-05-07 18:29:08 +0200

    Bundle JS dependencies

---
+++ index.js
@@ -0,0 +1,60 @@
+import { graphviz } from "@hpcc-js/wasm/dist/graphviz.es6";
+import svgPanZoom from "svg-pan-zoom";
+
+Elm.Main.init({ node: document.getElementById("root") });
+
+customElements.define(
+  "graphviz-dot",
+  class extends HTMLElement {
+    static get observedAttributes() {
+      return ["dot"];
+    }
+
+    attributeChangedCallback() {
+      this.update();
+    }
+
+    update() {
+      const dot = this.getAttribute("dot");
+      if (dot == null) {
+        this.innerHTML = "";
+        return;
+      }
+
+      // Render DOT into SVG.
+      graphviz
+        .dot(dot)
+        .then((svgString) => {
+          this.innerHTML = svgString;
+          const svgElement = this.firstElementChild;
+          if (svgElement instanceof SVGSVGElement) {
+            // Styling.
+            svgElement.style.width = "100%";
+            svgElement.style.height = "100%";
+            svgElement.style.cursor = "default";
+
+            // <title> elements are shown as tooltips in SVG. Graphviz
+            // generates titles such as “G” and “2->8” from the code that
+            // elm-graph generates. Those are just noisy. This is the only
+            // way I’ve found the get rid of them.
+            svgElement
+              .querySelectorAll("title")
+              .forEach((title) => title.remove());
+
+            // Enable drag-to-pan and scroll-to-zoom. Zoom out a little bit to
+            // avoid the graph touching the edges of the viewport, that looks better.
+            svgPanZoom(svgElement, {
+              dblClickZoomEnabled: false,
+            }).zoomBy(0.9);
+          }
+          return undefined;
+        })
+        .catch((error) => {
+          console.error(
+            "Failed to render using Graphviz and svg-pan-zoom",
+            error
+          );
+        });
+    }
+  }
+);
---
+++ prebuild.mjs
@@ -0,0 +1,15 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const DIR = path.dirname(fileURLToPath(import.meta.url));
+const BUILD = fileURLToPath(new URL("build", import.meta.url));
+
+fs.rmSync(BUILD, { recursive: true, force: true });
+
+fs.mkdirSync(BUILD);
+
+fs.copyFileSync(
+  `${DIR}/node_modules/@hpcc-js/wasm/dist/graphvizlib.wasm`,
+  `${BUILD}/graphvizlib.wasm`
+);
