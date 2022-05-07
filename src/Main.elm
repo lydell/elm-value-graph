9d5d52c2dcb26c52015ec52250a16c96a54a1374
Simon Lydell <simon.lydell@gmail.com>
2022-05-07 20:31:37 +0200

    Tweak info

--- src/Main.elm
+++ src/Main.elm
@@ -289,7 +289,7 @@ viewGraph values =
 
 viewInfo : List (Html msg)
 viewInfo =
-    [ Html.p [] [ Html.text "This app shows you how every function – or any value – in an Elm application depend on each other, as a graph. It lets you answer questions like “why do I depend on this package?”." ]
+    [ Html.p [] [ Html.text "This app shows you how every function – or any value – in an Elm application depend on each other, as a graph. It lets you answer questions like “why do I depend on this package?”. Think of it as a recursive “Find usages” IDE action." ]
     , Html.p [] [ Html.text "Note: All elm/* packages are excluded because they made the graph too noisy." ]
     , Html.p [] [ Html.text "Warning: This is essentially a quick hack. I run a bunch of regex on the compiled JavaScript output. It seems to work good enough, though." ]
     , Html.p [] [ Html.text "Source code: ", Html.a [ Html.Attributes.href "https://github.com/lydell/elm-value-graph" ] [ Html.text "github.com/lydell/elm-value-graph" ] ]
@@ -428,7 +428,7 @@ checkGraphSize dict =
                 ++ String.fromInt (Dict.size dict)
                 ++ " nodes. I only display graphs with "
                 ++ String.fromInt maxGraphSize
-                ++ " nodes or less. Otherwise the graph renderer (Graphviz) tends to hang."
+                ++ " nodes or less. Otherwise the graph renderer (Graphviz) tends to hang. Use the text input above to make the graph smaller."
 
     else
         Ok dict
